1733aeba7706aa7a78e7c9c8d2d7af7365f46882
Osiris-Matos <74211528+Osiris-Matos@users.noreply.github.com>
2023-11-30 20:20:49 -0400

    conecte la db al web host

--- src/db/db.php
+++ src/db/db.php
@@ -1,9 +1,9 @@
 <?php
-    $host = "containers-us-west-128.railway.app";
+    $host = "viaduct.proxy.rlwy.net";
     $username = "root";
-    $password = "MfAgNNKdgle56W4cHlAB";
-    $dbName = "libreria";
-    $port = 7423;
+    $password = "3FA6e13EcAEa-1a6g4-fadGFa2Ee-5d1";
+    $dbName = "railway";
+    $port = 15319;
 
     try {
         $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbName", $username, $password);
@@ -14,26 +14,5 @@
     }
 ?>
 
-<?php
-try {
-    // Ruta a la base de datos SQLite
-    $dbPath = 'ruta/a/tu/base-de-datos.db';
 
-    // Crear una nueva conexión a la base de datos SQLite
-    $db = new PDO('sqlite:' . $dbPath);
-
-    // Configurar para que PDO lance excepciones en caso de errores
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo 'Conexión exitosa a la base de datos SQLite';
-
-    // Puedes realizar consultas y otras operaciones aquí
-
-    // Cerrar la conexión cuando hayas terminado
-    $db = null;
-} catch (PDOException $e) {
-    // En caso de error, mostrar el mensaje de error
-    echo 'Error de conexión: ' . $e->getMessage();
-}
-?>
 
